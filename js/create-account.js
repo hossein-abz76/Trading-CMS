45fddb31a5d5baf73139c19f622398e2f9203e9e
hossein <habaszadeh76@gmail.com>
2023-04-10 15:05:07 +0430

    trading cms

---
+++ js/create-account.js
@@ -0,0 +1,5 @@
+const loginBtn = document.querySelector(".btn-login");
+loginBtn.href = "./login.html";
+
+const form = document.querySelector("form");
+form.action = "./login.html";
---
+++ js/login.js
@@ -0,0 +1,7 @@
+const createBtn = document.querySelector(".btn-create");
+createBtn.href = "./create-account.html";
+
+const form = document.querySelector("form");
+form.action = "./index.html";
+
+
